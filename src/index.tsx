e026b6978cd8dce773105bd55ac9ddc7f5add801
Samuel Façanha <samufacanha@gmail.com>
2023-04-09 13:04:09 -0300

    chore: update architecture to use Vite + SWC

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const container = document.getElementById('root');
-
-const app = createRoot(container!);
-
-app.render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
