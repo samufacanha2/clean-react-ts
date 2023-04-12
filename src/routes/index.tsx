e97c6aba408cd4cc7cca2c287dccb4f9041ed845
Samuel Façanha <samufacanha@gmail.com>
2023-04-11 22:45:33 -0300

    feat: added vitest unit tests

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
-import Home from 'pages/Home/';
+import Home from 'pages/Home';
 import About from 'pages/About';
 
 const Router: React.FC = () => {
